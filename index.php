2823b99352af93744f78ea5a527b865a67da6976
Jared Faucher <jared.faucher@gmail.com>
2014-07-01 10:41:52 -0400

    added form to index; modified css

--- index.php
+++ index.php
@@ -8,24 +8,41 @@
 	<body>
 		<div class="container">
 			<h1>Jared Faucher ::: P2</h1>
-			<h2>xkcd Password Generator</h2>
-			<p>
+			<h3>xkcd Password Generator</h3>
+			<br>
+			<p class="password">
 			<?php
 
 				$dictionary = file("dictionary.txt");  //http://www-01.sil.org/linguistics/wordlists/english/wordlist/wordsEn.txt
-
-				for ($i = 0; $i < 4; $i++)
+				if (!empty($_POST["numberOfWords"]))
+				{
+					$numberOfWords = $_POST["numberOfWords"];
+				}
+				else
+				{
+					$numberOfWords = 4;
+				}
+				for ($i = 0; $i < $numberOfWords; $i++)
 				{
 					${"word" . $i} = $dictionary[rand(1, 109583)];
 
 					print ${"word" . $i};
-					if ($i < 3)
+					if ($i < $numberOfWords - 1)
 					{
 						print "-";
 					}
 				}	
 			?>
 			</p>
+			<br>
+			<form action="index.php" method="post">
+				<label for="numberOfWords">Number of Words: </label>
+				<input maxlength="1" type="text" name="numberOfWords" id="numberOfWords" value>
+				(Max = 9)
+				<br>
+				<input type="submit" class="btn btn-default" value="Submit">
+			</form>
+			<br>
 			<a href="http://xkcd.com/936/">
 				<img src="http://imgs.xkcd.com/comics/password_strength.png" />
 			</a>
